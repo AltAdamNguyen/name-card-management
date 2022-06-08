29d1301a8843521f53470a800ba4278da3fd5dcd
AltAdamNguyen <altadamnguyen@gmail.com>
2022-06-09 02:21:46 +0700

    Update home screen

--- frontend/src/Screen/Home/styles.js
+++ frontend/src/Screen/Home/styles.js
@@ -2,6 +2,33 @@ import { StyleSheet } from 'react-native';
 
 // define your styles
 const styles = StyleSheet.create({
+    containerOverlay: {
+        fontfamily: 'Roboto',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(130, 130, 130,0.5)',
+    },
+
+        modalView: {
+            backgroundColor: '#ffff',
+            borderRadius: 10,
+            width: '50%',
+            alignItems: 'flex-start',
+        },
+        modalItem: {
+            flexDirection: 'row',
+            alignItems: 'center',
+        },
+            modalIcon: {
+                width: 32,
+                margin: 10
+            },
+            modalText: {
+                fontSize: 15,
+            },
+
     container: {
         fontfamily: 'Roboto',
         flex: 1,
@@ -11,129 +38,139 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffff',
     },
 
-        header: {
-            flex: 1.5,
-            flexDirection: 'column',
-            justifyContent: 'space-between',
+    header: {
+        flex: 1.5,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    sectionStyle: {
+        flex:1,
+        width: '90%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+        borderColor: '#F3F3F3',
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    icSearch: {
+        padding: 10,
+        margin: 5,
+        height: 16,
+        width: 16,
+        resizeMode: 'stretch',
+        alignItems: 'center'
+    },
+    input: {
+        flex: 1,
+        height: 40,
+        fontSize: 16,
+        color: '#828282',
+    },
+
+    titleContainer: {
+        flex: 1,
+        width: '90%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    labelList: {
+        textAlign: 'left',
+        width: '50%',
+        fontSize: 16,
+        fontWeight: 'bold',
+        
+    },
+    flag: {
+        width: '50%',
+    },
+        buttonFlag: {
+            borderRadius: 10,
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
             alignItems: 'center',
         },
-            sectionStyle: {
-                width: '90%',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 10,
-                borderColor: '#F3F3F3',
-                height: 40,
-                borderWidth: 1,
-                borderRadius: 10,
-                paddingLeft: 10,
-                paddingRight: 10,
+            sectionFlag: {
+                flexDirection:'row', 
+                alignItems:'center', 
+                borderRadius:10, 
+                paddingLeft: 5, 
+                paddingRight: 5
             },
-                icSearch: {
-                    padding: 10,
-                    margin: 5,
-                    height: 16,
-                    width: 16,
-                    resizeMode: 'stretch',
-                    alignItems: 'center'
-                },
-                input: {
-                    flex: 1,
-                    height: 40,
-                    fontSize: 16,
-                    color: '#828282',
-                },
-
-            titleContainer: {
-                flex: 1,
-                flexDirection: 'row',
-                width: '90%',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-            },
-                label: {
-                    flex: 1,
-                    textAlign: 'left',
-                    fontSize: 15,
+                labelFlag: {
                     fontWeight: 'bold',
-                    color: '#333333',
-                },
-                picker: {
-                    flex: 1,
-                    textAlign: 'right',
                     fontSize: 16,
-                    color: '#333333',
+                    marginRight: 10,
                 },
 
-        listContainer: {
-            flex: 8.5,
-            width: '90%',
-            flexDirection: 'column',
-        },
-            item: {
-                width: '100%',
-                height: 80,
-                borderWidth: 1,
-                borderColor: '#828282',
-                borderRadius: 10,
-                padding: 5,
-                marginBottom: 8,
-                alignItems: 'center',
-                flexDirection: 'row'
-            },
+    listContainer: {
+        flex: 8.5,
+        width: '90%',
+        flexDirection: 'column',
+    },
+    item: {
+        width: '100%',
+        height: 80,
+        borderWidth: 1,
+        borderColor: '#828282',
+        borderRadius: 10,
+        padding: 5,
+        marginBottom: 8,
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
 
-                bookmark: {
-                    bottom: '85%',
-                    left: '1%',
-                    tintColor: '#EB5757',
-                },
-                image: {
-                    top: '17%',
-                    width: 94.29,
-                    height: 70,
-                    borderRadius: 10,
-                },
-                txtContact: {
-                    flex: 1,
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'flex-start',
-                    marginLeft: 10,
-                },
-                title: {
-                    width: '100%',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                },
-                nameContact: { 
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                    color: '#262626',
-                },
-                titleContact: {
-                    fontSize: 12,
-                    color: '#8C8C8C',
-                },
-                companyContact: {
-                    fontSize: 12,
-                    color: '#8C8C8C',
-                },
-                date: {
-                    marginTop: 15,
-                    fontSize: 10,
-                },
-            floatButton: {
-                position: 'absolute',
-                width: 40,
-                height: 40,
-                alignItems: 'center',
-                justifyContent: 'center',
-                right: 10,
-                bottom: 20,
-                backgroundColor: '#1890FF',
-                borderRadius: 20,
-            },
+    image: {
+        width: 94.29,
+        height: 70,
+        borderRadius: 10,
+    },
+    txtContact: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        marginLeft: 10,
+    },
+    title: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    nameContact: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#262626',
+    },
+    titleContact: {
+        fontSize: 12,
+        color: '#8C8C8C',
+    },
+    companyContact: {
+        fontSize: 12,
+        color: '#8C8C8C',
+    },
+    date: {
+        marginTop: 15,
+        fontSize: 10,
+    },
+    floatButton: {
+        position: 'absolute',
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        right: 10,
+        bottom: 20,
+        backgroundColor: '#1890FF',
+        borderRadius: 25,
+    },
 
 
 });
