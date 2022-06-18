f8850c78f052b9d06e37cb8c041bf83eaa824a9e
Trung Dang <trungdang249@gmail.com>
2022-06-18 20:22:57 +0700

    Add jose-jwt

--- backend/NCMSystem/Controllers/UserController.cs
+++ backend/NCMSystem/Controllers/UserController.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.IdentityModel.Tokens;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Http;
-using JWT.Algorithms;
-using JWT.Builder;
+using Jose;
 using NCMSystem.Models;
 using NCMSystem.Models.CallAPI.User;
 
@@ -13,30 +12,32 @@ namespace NCMSystem.Controllers
     public class UserController : ApiController
     {
         private NCMSystemEntities db = new NCMSystemEntities();
+
         [HttpPost]
         [Route("api/auth/login")]
         public UserResponse Post([FromBody] UserRequest request)
         {
             string email = request.email;
             string password = request.password;
-            
+
             var user = db.users.FirstOrDefault(x => x.email == email && x.password == password);
 
-            var token = JwtBuilder.Create()
-                .WithAlgorithm(new RS256Algorithm(certificate))
-                .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
-                .AddClaim("claim1", 0)
-                .AddClaim("claim2", "claim2-value")
-                .Encode();
+            var payload = new Dictionary<string, object>()
+            {
+                { "email", email },
+                { "iat", DateTimeOffset.Now.ToUnixTimeSeconds() },
+                { "exp", DateTimeOffset.Now.ToUnixTimeSeconds() + 3600 }
+            };
+
+            Jwk key = new Jwk(Encoding.ASCII.GetBytes("eyJzdWIiOiJtci54QGNvbnRvc28uY29tIiwiaWF0IjoxNjU1NDY1NDE1LCJleHAiOjE2NTU0NjkwMTV9"));
+
+            string token = Jose.JWT.Encode(payload, key, JwsAlgorithm.HS256);
 
             Console.WriteLine(token);
             
-            
             UserResponse response = new UserResponse();
-            
+
             return response;
         }
-        
-        
     }
 }
