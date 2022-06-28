3437d820e557b9f266b3e566fee27f7ef9ad085c
Trung Dang <trungdang249@gmail.com>
2022-06-29 01:58:53 +0700

    Do Unit Test UserController

---
+++ backend/TestNCMSystem/UserControllerTest.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NCMSystem.Controllers;
+using NCMSystem.Models.CallAPI.User.RefreshToken;
+using NCMSystem.Models.CallAPI.User.UserLogin;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using static Newtonsoft.Json.Linq.JObject;
+
+namespace TestNCMSystem
+{
+    [TestFixture]
+    public class UserControllerTest
+    {
+        [Test]
+        public void UserController_Login_MissingEmailAndPassword()
+        {
+            //arrange
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            //act
+            var cc = controller.Login(new UserRequest()
+            {
+                
+            });
+            
+            //assert
+            Assert.AreEqual(cc.Response.StatusCode, HttpStatusCode.BadRequest);
+        }
+        
+        [Test]
+        public void UserController_Login_MissingEmail()
+        {
+            //arrange
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            //act
+            var cc = controller.Login(new UserRequest()
+            {
+                Password = "Tung88644264@"
+            });
+            
+            //assert
+            Assert.AreEqual(cc.Response.StatusCode, HttpStatusCode.BadRequest);
+        }
+        
+        [Test]
+        public void UserController_Login_MissingPassword()
+        {
+            //arrange
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            //act
+            var cc = controller.Login(new UserRequest()
+            {
+                Email = "nmtung.study@gmail.com"
+            });
+            
+            //assert
+            Assert.AreEqual(cc.Response.StatusCode, HttpStatusCode.BadRequest);
+        }
+        
+        [Test]
+        public void UserController_Login_UserNotFound()
+        {
+            //arrange
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            //act
+            var cc = controller.Login(new UserRequest()
+            {
+                Email = "nmtung.stuy@gmail.com",
+                Password = "Tung8864464@"
+            });
+            
+            //assert
+            Assert.AreEqual(cc.Response.StatusCode, HttpStatusCode.BadRequest);
+        }
+        
+        [Test]
+        public void UserController_Login_Success()
+        {
+            //arrange
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            //act
+            var cc = controller.Login(new UserRequest()
+            {
+                Email = "nmtung.study@gmail.com",
+                Password = "Tung88644264@"
+            });
+            
+            JObject json = Parse(cc.Response.Content.ReadAsStringAsync().Result);
+            //assert
+            Assert.AreEqual(cc.Response.StatusCode, HttpStatusCode.OK);
+            Assert.NotNull(json["data"]?["access_token"]);
+            Assert.NotNull(json["data"]?["refresh_token"]);
+        }
+        
+        [Test]
+        public void UserController_RefreshToken_MissingRefreshToken()
+        {
+            //arrange
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            //act
+            var cc = controller.PostRefreshToken(new RefreshTokenRequest()
+            {
+                
+            });
+            
+            //assert
+            Assert.AreEqual(cc.Response.StatusCode, HttpStatusCode.BadRequest);
+        }
+        
+        [Test]
+        public void UserController_RefreshToken_Success()
+        {
+            //arrange
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            //act
+            var cc = controller.PostRefreshToken(new RefreshTokenRequest()
+            {
+                RefreshToken = ""
+            });
+            
+            //assert
+            Assert.AreEqual(cc.Response.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
